b2c7499c435e7f6017a15ad2abc234212a6c860e
Łukasz Biedrzycki <lukasz.biedrzycki@gmail.com>
2021-06-14 20:53:29 +0200

    Add references to the template and and.book-list, first function

---
+++ src/js/script.js
@@ -0,0 +1,34 @@
+{
+  'use strict';
+
+  
+  const select = {
+    templateOf: {
+      bookTemplate: '#template-book',   //referencja do szablonu template oraz books-list
+    },
+    containerOf: {
+      booksList: '.books-list',
+    },
+  };
+
+  const templates = {
+    bookTemplate: Handlebars.compile(document.querySelector(select.templateOf.bookTemplate).innerHTML),
+  };
+
+  function renderInBooks() {
+    
+
+    
+    for (let book of dataSource.books) {  /*loop for every element - from dataSource.books*/
+    
+      const generatedHTML = templates.bookTemplate(book); /* generate HTML from the template and from data of the book */
+      
+      const element = utils.createDOMFromHTML(generatedHTML); /* generate DOM element based on the HTML */ 
+      
+      const booksListContainer = document.querySelector(select.containerOf.booksList);  /*find booksList container and append child to the .books-list*/
+      booksListContainer.appendChild(element);
+    }
+  }
+  renderInBooks();
+
+}
